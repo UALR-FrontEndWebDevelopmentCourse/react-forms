42f3367ee826e593611f61002e0446476a89d0b8
irconde <irconde@ualr.edu>
2023-03-22 08:39:19 -0500

    Store defined

--- src/store.js
+++ src/store.js
@@ -1,3 +1,15 @@
-export default function store() {
+import {createStore} from 'redux';
+import {combineForms, createForms} from 'react-redux-form';
 
-}
+const initialUserState = {
+    firstName: '',
+    lastName: ''
+};
+
+// If you want your entire store to have the form state...
+const store = createStore(combineForms({
+    use:initialUserState,
+}))
+
+
+export default store;
